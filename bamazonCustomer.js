c4e2675e18c9c7e417a53e2c9cc292c9947da7cd
Rob Conway <robconway12@gmail.com>
2019-05-16 21:30:15 -0700

    completed prompts to user and SQL query to check for quantity in database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,74 +1,70 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+const chosenItem = 0;
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "bamazonDB"
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'bamazonDB',
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  readProducts();
+	if (err) throw err;
+	console.log('connected as id ' + connection.threadId + '\n');
+	selectProducts();
 });
 
-function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-  }
+function selectProducts() {
+	console.log('Selecting all products...\n\n');
+	connection.query('SELECT * FROM products', function(err, results) {
+		if (err) throw err;
+    
+    inquirer
+			.prompt([
+				{
+					name: 'choice',
+					type: 'input',
+					choices: function() {
+						for (var i = 0; i < results.length; i++) {
+							console.log(
+                `${results[i].item_id} - ${results[i].product_name}\n$${results[i].price.toFixed(2)}\n--------------------------------`
+              );
+            }
+          },
+					message: 'Which product would you like to buy? [Enter ID #]',
+				},
+				{
+					name: 'units',
+					type: 'input',
+					message: 'How many units would you like?',
+				},
+			])
+			.then(function(answer) {
+					if (answer.choice <= results.length) {
+            verifyQuantity(answer.choice);
+					} else {
+            console.log(`${answer.choice} is not a valid item ID`)
+          }
+			})
+	});
+};
 
-  inquirer.prompt([
+function verifyQuantity(x) {
+  var query = "SELECT stock_quantity FROM products WHERE ?";
+  connection.query(query, { item_id: x}, function(err, res) {
+    if (err) throw err;
+    if (res >= x) {
+      fullfillment();
+    } else {
+    console.log('Insufficient quantity!');
+    connection.end();
+    }
+  });
+};
 
-    {
-      type: "input",
-      name: "name",
-      message: "Who are you???"
-    },
-  
-    {
-      type: "list",
-      name: "doingWhat",
-      message: "What are you doing in my house??",
-      choices: ["I made you cookies!", "No lie dude. I'm here to rob you.", "Uh. This is my house... Who are YOU???"]
-    },
-  
-    {
-      type: "checkbox",
-      name: "carryingWhat",
-      message: "What are you carrying in your hands??",
-      choices: ["TV", "Slice of Toast", "Butter Knife"]
-    },
-  
-    {
-      type: "confirm",
-      name: "canLeave",
-      message: "Can you leave now?"
-    },
-  
-    {
-      type: "password",
-      name: "myPassword",
-      message: "Okay fine. You can stay. But only if you say the magic password."
-    }
-  
-  ]).then(function(user) {
-  
-    // If the user guesses the password...
-    if (user.myPassword === "myHouse") {
-  
-      console.log("==============================================");
-      console.log("");
-      console.log("Well a deal's a deal " + user.name);
-      console.log("You can stay as long as you like.");
-      console.log("Just put down the " + user.carryingWhat.join(" and ") + ". It's kind of freaking me out.");
-      console.log("");
-      console.log("==============================================");
-    }
-  
+function fullfillment() {
+  console.log('it worked!');
+  connection.end();
+};
