5672c9c62b98df11464a38c67f48fd20232225e2
Rob Conway <robconway12@gmail.com>
2019-05-18 13:53:44 -0700

    added quantity check for inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-const chosenItem = 0;
+var confirm = require('inquirer-confirm');
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -20,8 +20,8 @@ function selectProducts() {
 	console.log('Selecting all products...\n\n');
 	connection.query('SELECT * FROM products', function(err, results) {
 		if (err) throw err;
-    
-    inquirer
+
+		inquirer
 			.prompt([
 				{
 					name: 'choice',
@@ -29,10 +29,12 @@ function selectProducts() {
 					choices: function() {
 						for (var i = 0; i < results.length; i++) {
 							console.log(
-                `${results[i].item_id} - ${results[i].product_name}\n$${results[i].price.toFixed(2)}\n--------------------------------`
-              );
-            }
-          },
+								`${results[i].item_id} - ${results[i].product_name}\n$${results[i].price.toFixed(
+									2
+								)}\n--------------------------------`
+							);
+						}
+					},
 					message: 'Which product would you like to buy? [Enter ID #]',
 				},
 				{
@@ -42,29 +44,43 @@ function selectProducts() {
 				},
 			])
 			.then(function(answer) {
-					if (answer.choice <= results.length) {
-            verifyQuantity(answer.choice);
-					} else {
-            console.log(`${answer.choice} is not a valid item ID`)
-          }
-			})
+				console.log('product choice: ' + answer.choice);
+				if (answer.choice <= results.length) {
+					verifyQuantity(answer.choice, answer.units);
+				} else {
+					console.log(`${answer.choice} is not a valid item ID`);
+					confirm('Would you like to try again?').then(
+						function confirmed() {
+							selectProducts();
+						},
+						function cancelled() {
+							console.log('Goodbye!');
+							connection.end();
+						}
+					);
+				}
+			});
 	});
-};
+}
 
-function verifyQuantity(x) {
-  var query = "SELECT stock_quantity FROM products WHERE ?";
-  connection.query(query, { item_id: x}, function(err, res) {
-    if (err) throw err;
-    if (res >= x) {
-      fullfillment();
-    } else {
-    console.log('Insufficient quantity!');
-    connection.end();
-    }
-  });
-};
+function verifyQuantity(userIDChoice, userQtyInput) {
+	var query = 'SELECT stock_quantity FROM products WHERE ?';
+	connection.query(query, { item_id: userIDChoice }, function(err, res) {
+		if (err) throw err;
+		if (res[0].stock_quantity >= userQtyInput) {
+			console.log('User Qty: '+ userQtyInput);
+			console.log('Available Qty: ' + res[0].stock_quantity);
+			fullfillment();
+		} else {
+			console.log('User Qty: '+ userQtyInput);
+			console.log('Available Qty: ' + res[0].stock_quantity);
+			console.log('Insufficient quantity!');
+			connection.end();
+		}
+	});
+}
 
 function fullfillment() {
-  console.log('it worked!');
-  connection.end();
-};
+	console.log('it worked!');
+	connection.end();
+}
